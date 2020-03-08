c68e540cbe8563588dc9972c335272a1b570a8b5
suryakc <apolloappstudio@gmail.com>
2020-03-08 19:23:21 +0530

    Fixed the servers setting.

--- src/main/java/com/suryakc/kafka/configuration/ConsumerConfiguration.java
+++ src/main/java/com/suryakc/kafka/configuration/ConsumerConfiguration.java
@@ -14,7 +14,7 @@ import java.util.Map;
 @Configuration
 public class ConsumerConfiguration {
 
-    private static final String KAFKA_BROKER = "localhost:9092;localhost:9093";
+    private static final String KAFKA_BROKER = "localhost:9092,localhost:9093";
     private static final String GROUP_ID = "kafka-sandbox";
 
     @Bean
